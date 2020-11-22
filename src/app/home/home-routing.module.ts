bcb087acdf09f8b4d49375ecd685972a93bed948
Ricardo Tadeu Sperlongo <ricardo_tadeu30@hotmail.com>
2020-11-22 13:38:50 -0300

    update home

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { OrderComponent } from '../order/order.component';
 import { HomeComponent } from './home.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent
+  },
+  {
+    path: 'order',
+    component: OrderComponent,
   }
 ];
 
