a06ffb2b397d2188859af2914b6430ff9e2aa8c3
rsperlongo <ricardo_tadeu30@hotmail.com>
2021-02-02 18:01:36 -0300

    feat: validation form cart

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -9,23 +9,34 @@ import { FormControl,FormGroup,Validators } from '@angular/forms';
 
 export class CartComponent implements OnInit {
   isCollapsed = true;
-
-  cartForm = new FormGroup({
-    email : new FormControl('', [Validators.required, Validators.email]),
-  })
+  cartForm: FormGroup;
 
 
   constructor() { }
 
   ngOnInit(): void {
-    this.getErrorMessage();
+    this.cartForm = new FormGroup({
+      cep: new FormControl('', [Validators.required]),
+      name: new FormControl('', [Validators.required, Validators.minLength(4)]),
+      address: new FormControl('', [Validators.required, Validators.minLength(4)]),
+      city: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      uf: new FormControl('', [Validators.required, Validators.maxLength(2)]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      phone: new FormControl('', [Validators.required, Validators.minLength(3)])
+    })
   }
+  get cep() { return this.cartForm.get('cep'); }
 
-  getErrorMessage() {
-    if (this.cartForm.hasError('required')) {
-      return 'Campo Obrigatório';
-    }
+  get name() { return this.cartForm.get('name'); }
 
-    return this.cartForm.hasError('email') ? 'Email não válido' : '';
-  }
+  get address() { return this.cartForm.get('address') }
+
+  get city() { return this.cartForm.get('city'); }
+
+  get uf() { return this.cartForm.get('uf'); }
+
+  get email() { return this.cartForm.get('email'); }
+
+  get phone() { return this.cartForm.get('phone'); }
+
 }
